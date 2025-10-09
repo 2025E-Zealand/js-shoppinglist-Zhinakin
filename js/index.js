f896e07b0243b38a08bfae42c683b0a8cbbcbc72
Zhinakin <>
2025-10-09 09:20:56 +0200

    ?

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+function plus(a,b){
+    return a + b
+}
+
+let result = plus(3,4)
+console.log(result)
+
+
+function findnode(id, type){
+    let node = document.getElementById
+    node.style.color = "red"
+    node.style.backgroundColor = "black"
+    node.style.fontSize = "30px"
+    node.style.fontWeight = "bold"
+    
+    let nynode = document.createElement(type)
+    nynode.innterText = "i am a new node"
+    nynode.style.color = "blue"
+    document.body.appendChild(nynode)
+
+    return node
+
+}
